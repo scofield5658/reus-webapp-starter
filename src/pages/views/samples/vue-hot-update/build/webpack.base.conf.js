a4a0cfb3aa1841428213b1df08e09454ca00305c
Guo <gggzzz@126.com>
2020-02-20 09:39:20 +0800

    [update] add pug-loader && stylus-lodaer

--- src/pages/views/samples/vue-hot-update/build/webpack.base.conf.js
+++ src/pages/views/samples/vue-hot-update/build/webpack.base.conf.js
@@ -40,6 +40,10 @@ module.exports = {
         include: [resolve("src"), resolve("test"), resolve("node_modules/webpack-dev-server/client")],
       },
       {
+        test: /\.pug/,
+        loader: "pug",
+      },
+      {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: "url-loader",
         options: {
--- src/pages/views/samples/vue-hot-update/src/App.vue
+++ src/pages/views/samples/vue-hot-update/src/App.vue
@@ -1,22 +1,17 @@
-<template>
-  <div id="app">
-    <mt-header :title="$route.name" :fixed="true"></mt-header>
-    <router-view />
-    <mt-tabbar v-if="isLogin" v-model="selected" :fixed="true">
-      <mt-tab-item id="/home">
-        <icon slot="icon" name="home" />
-        主页
-      </mt-tab-item>
-      <mt-tab-item id="/product">
-        <icon slot="icon" name="bookmark-o" />
-        产品
-      </mt-tab-item>
-      <mt-tab-item id="/mine">
-        <icon slot="icon" name="user-circle-o" />
-        我的
-      </mt-tab-item>
-    </mt-tabbar>
-  </div>
+<template lang="pug">
+  #app
+    mt-header(:title='$route.name', :fixed='true')
+    router-view
+    mt-tabbar(v-if='isLogin', v-model='selected', :fixed='true')
+      mt-tab-item(id='/home')
+        icon(slot='icon', name='home')
+        |主页
+      mt-tab-item(id='/product')
+        icon(slot='icon', name='bookmark-o')
+        |产品
+      mt-tab-item(id='/mine')
+        icon(slot='icon', name='user-circle-o')
+        |我的
 </template>
 
 <script>
@@ -84,15 +79,14 @@ export default {
 };
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: left;
-  padding: 0;
-  margin: 0;
-  margin-top: 40px;
-  color: #2c3e50;
-}
+<style lang="stylus">
+#app
+  font-family: 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: left
+  padding: 0
+  margin: 0
+  margin-top: 40px
+  color: #2c3e50
 </style>
